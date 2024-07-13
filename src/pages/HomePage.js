5943dbafd2bfd09f2aa99f8a7d91202fe24cf1e4
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-12 21:22:16 -0400

    search and display books

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+
+import SearchBooks from "../components/SearchBooks";
+
+
+const HomePage = () => {
+
+    return (
+        <>
+            <h1>Bookshelf</h1>
+            <h4>My Books</h4>
+            <SearchBooks />
+
+        </>
+
+
+    )
+};
+
+export default HomePage;
